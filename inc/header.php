cd48c1e95d782516485fb97a0acc0ac310222e78
kimyh5392 <mk.kimyh@gmail.com>
2017-11-13 20:22:35 +0900

    메인 퍼블리싱 작업중 . . .

---
+++ inc/header.php
@@ -0,0 +1,108 @@
+<!-- Header --->
+<header id="header">
+    <h1 class="logo">
+        <a href="/"><img src="/assets/images/logo.png" alt=""></a>
+    </h1>
+    <div class="quick-menu">
+        <ul>
+            <li class="visible-lg">
+                <a href="#">
+                    <i class="icon-user">
+                        <span class="sr-only"></span>
+                    </i>
+                </a>
+            </li>
+            <li class="visible-lg">
+                <a href="#">
+                    <i class="icon-share">
+                        <span class="sr-only"></span>
+                    </i>
+                </a>
+            </li>
+            <li>
+                <button class="btn btn-link btn-open-side-nav">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="sr-only">메뉴 열기</span>
+                </button>
+            </li>
+        </ul>
+    </div>
+
+    <!-- gnb-nav -->
+    <nav id="nav" class="visible-lg">
+        <div class="container">
+            <ul class="gnb-menu">
+                <li><a href="#">화이트타이거즈 소개</a></li>
+                <li><a href="#">화이트타이거즈 활동</a></li>
+                <li><a href="#">화이트타이거즈 소식</a></li>
+                <li><a href="#">응원활동</a></li>
+            </ul>
+        </div>
+    </nav>
+
+</header>
+<!-- //Header --->
+
+<!-- side-nav -->
+<div id="side-nav" role="presentation">
+    <div class="nav-backdrop"></div>
+    <div class="nav-content">
+        <div class="nav-header">
+            <h2 class="nav-logo">
+                <a href="/"><img src="/assets/images/logo.png" alt=""></a>
+            </h2>
+            <button type="button" class="btn btn-close">
+                <img src="/assets/images/icon_btn_close.png" alt="메뉴 닫기">
+            </button>
+        </div>
+        <div class="nav-body">
+            <nav class="nav">
+                <ul>
+                    <li>
+                        <a href="#">화이트타이거즈 소개</a>
+                        <ul>
+                            <li><a href="#">화이트타이거즈 이념<span></span></a></li>
+                            <li><a href="#">화이트타이거즈 역사<span></span></a></li>
+                            <li><a href="#">화이트타이거즈 조직<span></span></a></li>
+                            <li><a href="#">브랜드.저작권<span></span></a></li>
+                            <li><a href="#">Contact Us<span></span></a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">화이트타이거즈 활동</a>
+                        <ul>
+                            <li><a href="#">화이트타이거즈 이념<span></span></a></li>
+                            <li><a href="#">화이트타이거즈 역사<span></span></a></li>
+                            <li><a href="#">화이트타이거즈 조직<span></span></a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">화이트타이거즈 소식</a>
+                        <ul>
+                            <li><a href="#">화이트타이거즈 이념<span></span></a></li>
+                            <li><a href="#">화이트타이거즈 역사<span></span></a></li>
+                            <li><a href="#">화이트타이거즈 조직<span></span></a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">응원활동</a>
+                        <ul>
+                            <li><a href="#">화이트타이거즈 이념<span></span></a></li>
+                            <li><a href="#">화이트타이거즈 역사<span></span></a></li>
+                            <li><a href="#">화이트타이거즈 조직<span></span></a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <div class="btn-area">
+                <p>
+                    <a href="#" class="btn btn-white" role="button">회원가입</a>
+                    <a href="#" class="btn btn-white" role="button">로그인</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- //side-nav -->
--- index.php
+++ index.php
@@ -1,21 +1,240 @@
-<!DOCTYPE html>
-<html lang="ko">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>화이트타이거즈응원단</title>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/main.css">
+</head>
+<body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    
+    <!-- content Start --->
+    <main id="content">
+        
+        <!-- main visual slider -->
+        <div class="jumbotron">
+            <h2 class="jumbo-title">YOU GO<br>WE GO!</h2>
+        </div>
+        
+        <!-- entertainer-cheer -->
+        <article class="entertainer-cheer">
+            <div class="item">
+                <div class="details">
+                    <p>연예인 응원 영상</p>
+                    <div class="btn-more">
+                        <a href="#" class="btn btn-default" role="button">보러가기</a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="details">
+                    <p>연예인 응원 영상</p>
+                    <div class="btn-more">
+                        <a href="#" class="btn btn-default" role="button">보러가기</a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="details">
+                    <p>연예인 응원 영상</p>
+                    <div class="btn-more">
+                        <a href="#" class="btn btn-default" role="button">보러가기</a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="details">
+                    <p>연예인 응원 영상</p>
+                    <div class="btn-more">
+                        <a href="#" class="btn btn-default" role="button">보러가기</a>
+                    </div>
+                </div>
+            </div>
+        </article>
+        
+        
+        <!-- instagram -->
+        <section class="instagram">
+            <div class="section-header">
+                <div class="container">
+                    <h3 class="section-title">#WHITE_TIGERS  ON  INSTAGRAM</h3>
+                    <p>
+                        많은 사람 속에서 다채롭게 채워진<br class="visible-xs"> 화이트타이거즈 소식
+                    </p>
+                </div>
+            </div>
+            <div class="section-content">
+                <article>
+                    <div class="insta-list">
+                        <ul>
+                            <li>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img01.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img02.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img03.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="big-item">
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_big_img01.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
 
-        <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    </head>
-    <body>
-        <h1>Hello, world!</h1>
+                                <div class="item inline-box r-space">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img04.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                                <div class="item inline-box l-space">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img05.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img08.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img07.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img06.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-    </body>
+                    <div class="insta-list visible-md visible-lg">
+                        <ul>
+                            <li>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img01.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img02.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img03.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="big-item">
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_big_img01.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+
+                                <div class="item inline-box r-space">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img04.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                                <div class="item inline-box l-space">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img05.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img08.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img07.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/main/event_small_img06.jpg" class="img-responsive" alt="">
+                                    </figure>
+                                    <div class="content">
+                                        123123123
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </article>
+            </div>
+        </section>
+        
+    </main>
+    <!-- //content End --->
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
 </html>
