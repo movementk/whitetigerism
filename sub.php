244b7d52e6532721c44d957e5680d427107d85ea
kimyh5392 <mk.kimyh@gmail.com>
2017-11-15 09:04:03 +0900

    메인 레이아웃 퍼블리싱 작업중 . . .

---
+++ sub.php
@@ -0,0 +1,21 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/main.css">
+</head>
+<body class=""> <!-- 폳더별 body 에 Class 추가 -->
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    
+    <!-- content Start --->
+    <main id="content"> <!-- 페이지 명 클래스 추가  -->
+        
+        <!-- Container 디자인 여백업이 이미지나 효과가 100% 일때 사용 안함 -->
+        <div class="container"> 
+                        
+        </div>
+        
+    </main>
+    <!-- //content End --->
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
